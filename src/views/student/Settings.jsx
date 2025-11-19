46456ab422cd5d2275a673139a75df2b66f0c401
Le0d3v <harrywilsonth01@gmail.com>
2025-11-19 11:23:14 -0600

    Ajustando Padding en Opciones

--- src/views/student/Settings.jsx
+++ src/views/student/Settings.jsx
@@ -7,7 +7,7 @@ export default function Settings() {
   return (
     <div className="">
       <h1 className="text-center text-3xl font-black">Configuración</h1>
-      <div className="mt-5 px-8">
+      <div className="mt-5 md:px-8 p-1">
         <DatosPersonales />
         <CambiarPassword />
       </div>
