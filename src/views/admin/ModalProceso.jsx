eb0b89c9b9d13f63e396ae0309ed0dc9e6086504
Le0d3v <harrywilsonth01@gmail.com>
2025-11-20 00:27:15 -0600

    Finalizando frontend para gestion de un proceso 2

--- src/views/admin/ModalProceso.jsx
+++ src/views/admin/ModalProceso.jsx
@@ -19,7 +19,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
     <div className="fixed inset-0 bg-black/70 flex justify-center items-center z-50">
       <div
         className="bg-white w-full max-w-7xl p-6 rounded-xl shadow-xl text-black 
-             max-h-[90vh] overflow-y-auto"
+        max-h-[90vh] overflow-y-auto"
       >
         <h2 className="text-3xl font-black mb-3 text-emerald-400 text-center">
           Detalles del Proceso (
@@ -37,21 +37,29 @@ export default function ModalProceso({ open, onClose, proceso }) {
           <h2 className="text-center my-3 text-emerald-400 font-bold text-xl">
             Actividades:
           </h2>
+
           <div className="flex flex-col md:flex-row gap-5 w-full">
-            <div className="p-3 border-2 border-emerald-400 rounded-lg w-full">
+            {/* TARJETA 1 */}
+            <div
+              className="p-5 border border-emerald-300 rounded-xl w-full shadow-md 
+            hover:shadow-lg transition-all bg-gray-100 hover:-translate-y-1"
+            >
               <h2 className="text-center text-emerald-500 font-bold text-2xl">
                 Validación de Memoria de Estadía
               </h2>
+
               <div className="my-2 flex gap-1 items-center">
                 <p>Estado: </p>
                 <Estado estado={proceso.proceso.validacion_memoria_estadia} />
               </div>
+
               {proceso?.archivo?.memoria_estadia === null ||
               proceso?.archivo?.memoria_estadia === undefined ? (
                 <Alerta>No hay archivo adjuntado</Alerta>
               ) : (
                 <>
                   <p className="my-3 text-gray-600">Archivo Adjunto:</p>
+
                   <PDFViewer
                     url={
                       baseURL +
@@ -59,10 +67,12 @@ export default function ModalProceso({ open, onClose, proceso }) {
                       proceso.archivo.memoria_estadia
                     }
                   />
+
                   <div className="mt-5">
                     <h2 className="text-center text-blue-400 font-bold">
                       Acciones
                     </h2>
+
                     <div className="flex justify-center p-1">
                       <div className="flex gap-1">
                         <form onSubmit={handleSubmitAprobarMemoria}>
@@ -70,7 +80,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-red-500 text-white font-bold cursor-pointer
-                                  hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -81,12 +91,13 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             )}
                           </button>
                         </form>
+
                         <form onSubmit={handleSubmitAprobarMemoria}>
                           <button
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-green-500 text-white font-bold cursor-pointer
-                                  hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -103,40 +114,61 @@ export default function ModalProceso({ open, onClose, proceso }) {
                 </>
               )}
             </div>
-            <div className="p-3 border-2 border-emerald-400 rounded-lg w-full">
+
+            {/* TARJETA 2 */}
+            <div
+              className="p-5 border border-emerald-300 rounded-xl w-full shadow-md 
+            hover:shadow-lg transition-all bg-gray-100 hover:-translate-y-1"
+            >
               <h2 className="text-center text-emerald-500 font-bold text-2xl">
                 Validación de Datos Personales (Estudiante)
               </h2>
+
               <div className="my-2 flex gap-1 items-center">
                 <p>Estado: </p>
                 <Estado estado={proceso.proceso.validacion_datos_personales} />
               </div>
             </div>
-            <div className="p-3 border-2 border-emerald-400 rounded-lg w-full">
+
+            {/* TARJETA 3 */}
+            <div
+              className="p-5 border border-emerald-300 rounded-xl w-full shadow-md 
+            hover:shadow-lg transition-all bg-gray-100 hover:-translate-y-1"
+            >
               <h2 className="text-center text-emerald-500 font-bold text-2xl">
                 Realización de Encuesta de Egresados
               </h2>
+
               <div className="my-2 flex gap-1 items-center">
                 <p>Estado: </p>
                 <Estado estado={proceso.proceso.encuesta_egresados} />
               </div>
             </div>
           </div>
+
+          {/* GRUPO 2 */}
           <div className="flex flex-col md:flex-row gap-5 w-full mt-5">
-            <div className="p-3 border-2 border-emerald-400 rounded-lg w-full">
+            {/* TARJETA 4 */}
+            <div
+              className="p-5 border border-emerald-300 rounded-xl w-full shadow-md 
+            hover:shadow-lg transition-all bg-gray-100 hover:-translate-y-1"
+            >
               <h2 className="text-center text-emerald-500 font-bold text-2xl">
                 Comprobante de Pago por Donación de Inmobiliaria
               </h2>
+
               <div className="my-2 flex gap-1 items-center">
                 <p>Estado: </p>
                 <Estado estado={proceso.proceso.pago_donacion} />
               </div>
+
               {proceso?.archivo?.comprobante_donacion === null ||
               proceso?.archivo?.comprobante_donacion === undefined ? (
                 <Alerta>No hay archivo adjuntado</Alerta>
               ) : (
                 <>
                   <p className="my-3 text-gray-600">Archivo Adjunto:</p>
+
                   <PDFViewer
                     url={
                       baseURL +
@@ -144,10 +176,12 @@ export default function ModalProceso({ open, onClose, proceso }) {
                       proceso.archivo.comprobante_donacion
                     }
                   />
+
                   <div className="mt-5">
                     <h2 className="text-center text-blue-400 font-bold">
                       Acciones
                     </h2>
+
                     <div className="flex justify-center p-1">
                       <div className="flex gap-1">
                         <form onSubmit={handleSubmitAprobarMemoria}>
@@ -155,7 +189,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-red-500 text-white font-bold cursor-pointer
-                                  hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -166,12 +200,13 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             )}
                           </button>
                         </form>
+
                         <form onSubmit={handleSubmitAprobarMemoria}>
                           <button
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-green-500 text-white font-bold cursor-pointer
-                                  hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -188,14 +223,21 @@ export default function ModalProceso({ open, onClose, proceso }) {
                 </>
               )}
             </div>
-            <div className="p-3 border-2 border-emerald-400 rounded-lg w-full">
+
+            {/* TARJETA 5 */}
+            <div
+              className="p-5 border border-emerald-300 rounded-xl w-full shadow-md 
+            hover:shadow-lg transition-all bg-gray-100 hover:-translate-y-1"
+            >
               <h2 className="text-center text-emerald-500 font-bold text-2xl">
                 Carga de Imágen para Titulo Universitario
               </h2>
+
               <div className="my-2 flex gap-1 items-center">
                 <p>Estado: </p>
                 <Estado estado={proceso.proceso.carga_imagen} />
               </div>
+
               {proceso?.archivo?.imagen_titulacion === null ||
               proceso?.archivo?.imagen_titulacion === undefined ? (
                 <div className="mt-5">
@@ -204,6 +246,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
               ) : (
                 <>
                   <p className="my-3 text-gray-600">Archivo Adjunto:</p>
+
                   <img
                     src={
                       baseURL +
@@ -211,12 +254,14 @@ export default function ModalProceso({ open, onClose, proceso }) {
                       proceso?.archivo?.imagen_titulacion
                     }
                     alt="imagen-titulacion"
-                    className="w-full h-72"
+                    className="w-full h-72 rounded-md object-cover shadow"
                   />
+
                   <div className="mt-5">
                     <h2 className="text-center text-blue-400 font-bold">
                       Acciones
                     </h2>
+
                     <div className="flex justify-center p-1">
                       <div className="flex gap-1">
                         <form onSubmit={handleSubmitAprobarMemoria}>
@@ -224,7 +269,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-red-500 text-white font-bold cursor-pointer
-                                  hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -235,12 +280,13 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             )}
                           </button>
                         </form>
+
                         <form onSubmit={handleSubmitAprobarMemoria}>
                           <button
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-green-500 text-white font-bold cursor-pointer
-                                  hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -257,14 +303,21 @@ export default function ModalProceso({ open, onClose, proceso }) {
                 </>
               )}
             </div>
-            <div className="p-3 border-2 border-emerald-400 rounded-lg w-full">
+
+            {/* TARJETA 6 */}
+            <div
+              className="p-5 border border-emerald-300 rounded-xl w-full shadow-md 
+            hover:shadow-lg transition-all bg-gray-100 hover:-translate-y-1"
+            >
               <h2 className="text-center text-emerald-500 font-bold text-2xl">
                 Registro de Referencia de Pago por Titulo Universitario
               </h2>
+
               <div className="my-2 flex gap-1 items-center">
                 <p>Estado: </p>
                 <Estado estado={proceso.proceso.pago_titulo} />
               </div>
+
               {proceso?.archivo?.referencia_pago === null ||
               proceso?.archivo?.referencia_pago === undefined ? (
                 <div className="mt-5">
@@ -273,13 +326,16 @@ export default function ModalProceso({ open, onClose, proceso }) {
               ) : (
                 <>
                   <p className="my-3 text-gray-600">Referencia Enviada:</p>
+
                   <p className="mt-1 p-2 bg-emerald-400 text-white font-bold rounded">
                     {proceso?.archivo?.referencia_pago}
                   </p>
+
                   <div className="mt-5">
                     <h2 className="text-center text-blue-400 font-bold">
                       Acciones
                     </h2>
+
                     <div className="flex justify-center p-1">
                       <div className="flex gap-1">
                         <form onSubmit={handleSubmitAprobarMemoria}>
@@ -287,7 +343,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-red-500 text-white font-bold cursor-pointer
-                                  hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -298,12 +354,13 @@ export default function ModalProceso({ open, onClose, proceso }) {
                             )}
                           </button>
                         </form>
+
                         <form onSubmit={handleSubmitAprobarMemoria}>
                           <button
                             type="submit"
                             disabled={memoriaLoading}
                             className="px-2 py-1 rounded bg-green-500 text-white font-bold cursor-pointer
-                                  hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
+                            hover:bg-green-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center w-28"
                           >
                             {memoriaLoading ? (
                               <ClipLoader color="#ffffff" size={24} />
@@ -328,7 +385,7 @@ export default function ModalProceso({ open, onClose, proceso }) {
             type="button"
             onClick={onClose}
             className="px-2 py-1 rounded bg-red-500 text-white font-bold cursor-pointer
-                  hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center"
+            hover:bg-red-600 hover:-translate-y-1 transition flex gap-1 items-center justify-center"
           >
             <X size={18} /> Cerrar
           </button>
